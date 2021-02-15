c60732bb8c606127b634896fe455607356d550f5
Steven Zhang <YZhang@lcs.on.ca>
2021-02-15 11:33:36 -0500

    Finish enumeration

---
+++ Enumeration.swift
@@ -0,0 +1,14 @@
+//
+//  Enumeration.swift
+//  CoverterApp
+//
+//  Created by Steven Zhang on 2021-02-15.
+//
+
+import Foundation
+
+enum TempuretureUnits {
+    case Celsius = °C
+    case Fahrenheit = °F
+    case Kelvin = K
+}
